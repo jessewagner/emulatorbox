81dd0dd04418244a5b477c4c598ecdf29513a266
Jesse <wagner7@pdx.edu>
2014-11-15 16:16:24 -0800

    changed icon for program to snes controller

--- emulator_program/EMULATORBOX.Designer.cs
+++ emulator_program/EMULATORBOX.Designer.cs
@@ -38,6 +38,7 @@
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -45,7 +46,6 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
@@ -157,71 +157,9 @@
             this.toolTip1.SetToolTip(this.pictureBox4, "Start the emulator for the NEOGEO and Capcom Arcade Games");
             this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
             // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(32, 254);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(277, 20);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "Nintendo Entertainment System";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(342, 254);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(277, 20);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "Super Nintendo Entertainment System";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label3
-            // 
-            this.label3.Location = new System.Drawing.Point(645, 254);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(262, 20);
-            this.label3.TabIndex = 13;
-            this.label3.Text = "Nintendo 64";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label4
-            // 
-            this.label4.Location = new System.Drawing.Point(32, 525);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(277, 20);
-            this.label4.TabIndex = 14;
-            this.label4.Text = "GameBoy and GameBoy Advance";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label5
-            // 
-            this.label5.Location = new System.Drawing.Point(342, 525);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(277, 20);
-            this.label5.TabIndex = 15;
-            this.label5.Text = "Nintendo DS";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label6
-            // 
-            this.label6.Location = new System.Drawing.Point(32, 804);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(277, 20);
-            this.label6.TabIndex = 16;
-            this.label6.Text = "Sega Genesis";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label7
-            // 
-            this.label7.Location = new System.Drawing.Point(342, 804);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(277, 20);
-            this.label7.TabIndex = 17;
-            this.label7.Text = "NEOGEO and Capcom Arcade Games";
-            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // button1
             // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.button1.Location = new System.Drawing.Point(645, 298);
             this.button1.Name = "button1";
@@ -231,6 +169,76 @@
             this.toolTip1.SetToolTip(this.button1, "Click this button to close the application");
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.Location = new System.Drawing.Point(32, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Nintendo Entertainment System";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.Location = new System.Drawing.Point(342, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Super Nintendo Entertainment System";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.Location = new System.Drawing.Point(645, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(262, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Nintendo 64";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.Location = new System.Drawing.Point(32, 525);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(277, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "GameBoy and GameBoy Advance";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.Location = new System.Drawing.Point(342, 525);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(277, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Nintendo DS";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label6.Location = new System.Drawing.Point(32, 804);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(277, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Sega Genesis";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label7.Location = new System.Drawing.Point(342, 804);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(277, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "NEOGEO and Capcom Arcade Games";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // EMULATORBOX
             // 
@@ -254,6 +262,7 @@
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "EMULATORBOX";
             this.Text = "EMULATORBOX";
             this.toolTip1.SetToolTip(this, "Click the picture of the device you would like to play!");
